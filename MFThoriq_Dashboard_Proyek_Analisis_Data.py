9fb3d9d639668e22bd1773251343ff8736af70f3
FathThoriq <109966776+FathThoriq@users.noreply.github.com>
2024-12-24 20:36:24 +0800

    Update MFThoriq_Dashboard_Proyek_Analisis_Data.py

--- MFThoriq_Dashboard_Proyek_Analisis_Data.py
+++ MFThoriq_Dashboard_Proyek_Analisis_Data.py
@@ -30,7 +30,7 @@ rental_hour = hour_df.groupby("hr")["cnt"].mean()
 low_threshold = rental_hour.quantile(0.33)
 high_threshold = rental_hour.quantile(0.67)
 rental_hour_cluster = rental_hour.apply(
-  lambda x: "Low Usage" if x <= low_threshold else "High Usage" if x > high_threshold else "Medium Usage"
+  lambda x: "Penggunaan Rendah" if x <= low_threshold else "Penggunaan Tinggi" if x > high_threshold else "Penggunaan Sedang"
 )
 
 # Visualisasi Clustering
